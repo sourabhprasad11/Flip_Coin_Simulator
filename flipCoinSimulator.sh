4c9b315987f0731246777fbb7b87d762ceb673ca
sourabhprasad11 <sourabh.prasad444@gmail.com>
2020-10-12 00:57:32 +0530

    Multiple Flipping of the coin

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,8 +1,19 @@
 #! /bin/bash
 echo "Welcome to the FLIP COIN SIMULATOR"
+i=0
+ch=0
+ct=0
+while [ $i -lt 10 ]
+do
 x=$((RANDOM%2));
 if [ $x -eq 1 ]; then
    echo "Heads"
+   ((ch++))
 else
    echo "Tails"
+   ((ct++))
 fi
+((i++))
+done
+echo "Heads= $ch"
+echo "Tails= $ct"
