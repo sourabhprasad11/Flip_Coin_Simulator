fc18a1edc59ae1d4f72b1bc4cd14716bd39ef98d
sourabhprasad11 <sourabh.prasad444@gmail.com>
2020-10-12 01:15:48 +0530

    Multiple Coin Simulation Showing Win or Tie

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -3,7 +3,8 @@ echo "Welcome to the FLIP COIN SIMULATOR"
 i=0
 ch=0
 ct=0
-while [ $i -lt 10 ]
+z=0
+while [[ $ch -lt 21 && $ct -lt 21 ]]
 do
 x=$((RANDOM%2));
 if [ $x -eq 1 ]; then
@@ -17,3 +18,12 @@ fi
 done
 echo "Heads= $ch"
 echo "Tails= $ct"
+if [ $ch -gt $ct ]; then
+	z=$(( $ch-$ct ))
+	echo "Player choosing Heads won by: $z"
+elif [ $ch -eq $ct ]; then
+	echo "Tie"
+elif [ $ch -lt $ct]; then
+	z=$(( $ct-$ch ))
+	echo "Player choosing Tails won by: $z"
+fi
