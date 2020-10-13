921a4d10a9d718444d2c40a24301f325afee51e6
sourabhprasad11 <sourabh.prasad444@gmail.com>
2020-10-13 11:17:37 +0530

    Solved Tie by getting a minimum difference of 2

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 echo "Welcome to the FLIP COIN SIMULATOR"
-i=0
+q=0
 ch=0
 ct=0
 z=0
-while [[ $ch -lt 21 && $ct -lt 21 ]]
+while [ $q -lt 20 ]
 do
 x=$((RANDOM%2));
 if [ $x -eq 1 ]; then
@@ -14,16 +14,45 @@ else
    echo "Tails"
    ((ct++))
 fi
-((i++))
+((q++))
 done
+
 echo "Heads= $ch"
 echo "Tails= $ct"
+
+
 if [ $ch -gt $ct ]; then
 	z=$(( $ch-$ct ))
 	echo "Player choosing Heads won by: $z"
 elif [ $ch -eq $ct ]; then
-	echo "Tie"
-elif [ $ch -lt $ct]; then
+	echo "Tie Situation"
+   h=0
+   t=0
+   diff=0
+   while [[ $diff -lt 2 ]]
+   do
+      y=$((RANDOM%2));
+      if [ $y -eq 1 ]; then
+         echo "Heads"
+         ((h++))
+      else
+         echo "Tails"
+         ((t++))
+      fi
+      if [ $h -gt $t ]; then
+      diff=$(($h-$t))   #head-tail
+      echo "Head - tail=" $diff ;
+      else
+      diff=$(($t-$h))  #tail-head
+      echo "Tail - head=" $diff ;
+
+      fi
+   done
+   echo "Tie Situation solved - Player with Last Call wins "
+
+
+
+elif [ $ch -lt $ct ]; then
 	z=$(( $ct-$ch ))
 	echo "Player choosing Tails won by: $z"
 fi
